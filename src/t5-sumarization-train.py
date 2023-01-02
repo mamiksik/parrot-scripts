cf5f5d039717e78b5d6cc5063d6be07b6ea45ae0
mamiksik <mamiksik@gmail.com>
2023-01-02 10:59:44 +0100

    wip - summarize

--- src/t5-sumarization-train.py
+++ src/t5-sumarization-train.py
@@ -20,7 +20,7 @@ wandb.init(config=hyperparameter_defaults, project="CommitPredictorT5")
 
 def preprocess(tokenizer: RobertaTokenizer, examples):
 
-    messages = [f"Generate commit message:\n{patch}" for patch in examples["patch"]]
+    messages = [f"summarize:\n{patch}" for patch in examples["patch"]]
     model_inputs = tokenizer(
         messages, max_length=412, padding="max_length", truncation=True
     )
@@ -151,7 +151,7 @@ def main():
     trainer.train()
 
     print(f"🚀  Pushing model to HuggingFace Hub")
-    commit_id = trainer.push_to_hub(f"End of training (patience=7, prefix) {wandb.run.name}", blocking=True)
+    commit_id = trainer.push_to_hub(f"End of training (patience=7, prefix=summarize) {wandb.run.name}", blocking=True)
     print(f"🎉  Model pushed to HuggingFace Hub: {commit_id}")
 
     print(f"🏁  Training Done")
