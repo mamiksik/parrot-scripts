eeeb21a21f642722e180dde69f3b146b3c258851
mamiksik <mamiksik@gmail.com>
2023-01-27 16:19:52 +0100

    wip

--- src/t5-sumarization-train-pl.py
+++ src/t5-sumarization-train-pl.py
@@ -202,8 +202,8 @@ def main():
     if not training_args.debug:
         model.model.save_pretrained(model_output_path)
         new_model = T5ForConditionalGeneration.from_pretrained(model_output_path)
-        new_model.push_to_hub("T5-commit-message-generator", commit_message=f"Experiment ID: {wandb_logger.experiment.id}")
-        tokenizer.push_to_hub("T5-commit-message-generator", commit_message=f"Experiment ID: {wandb_logger.experiment.id}")
+        new_model.push_to_hub("mamiksik/T5-commit-message-generator", commit_message=f"Experiment ID: {wandb_logger.experiment.id}")
+        tokenizer.push_to_hub("mamiksik/T5-commit-message-generator", commit_message=f"Experiment ID: {wandb_logger.experiment.id}")
 
 
 if __name__ == "__main__":
